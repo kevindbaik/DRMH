e88f1492b89f3a100ef337f3104aa1a5450ebc88
kevin <kevindbaik@gmail.com>
2024-03-01 22:25:01 -0800

    dont need super

--- src/main/java/learn/mastery/data/DataException.java
+++ src/main/java/learn/mastery/data/DataException.java
@@ -1,12 +1,8 @@
 package learn.mastery.data;
 
 public class DataException extends Throwable {
-    public DataException(String message) {
-        super();
-    }
-
     public DataException(Throwable cause) {
         super();
     }
-    
+
 }
